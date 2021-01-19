1ab973f8e6a019bf2c0318c173f80cce7faebaa9
Rakitzis45 <michael.rakitzis@gmail.com>
2021-01-19 11:17:00 -0500

    commit tree 256371b27f70700a4f0ba6532167ae1725656c37

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, string){
+    return array.filter(n => 
+        n.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, string){
+    return array.filter(n => 
+       n[0] === string[0]
+    )
+}
+
+function matchName(array, string){
+    return array.filter(n =>
+        n.name === string
+    )
+}
